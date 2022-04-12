3546c0176210705ae49125f773be2442c4511df7
alan <alannavaseg_02q@hotmail.com>
2022-04-12 13:59:27 -0500

    new comment 2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    #this does not change the code
+    #this does not change the code 
     name="limbus",
     packages=find_packages(),
     version="0.0.1",
